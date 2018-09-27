b16b158bee5fb9411838d043672cac3f22b0a4f3
siennad <siennad97@gmail.com>
2018-09-27 22:53:41 +0300

    fixed merged errors.

--- symfonyApp/src/Entity/StudentAnswer.php
+++ symfonyApp/src/Entity/StudentAnswer.php
@@ -22,47 +22,6 @@ class StudentAnswer
      */
     private $questionInExam;
 
-    public function getId(): ?int
-    {
-        return $this->id;
-    }
-
-    public function getQuestionInExam(): ?QuestionInExam
-    {
-        return $this->questionInExam;
-    }
-
-    public function setQuestionInExam(?QuestionInExam $questionInExam): self
-    {
-        $this->questionInExam = $questionInExam;
-
-        return $this;
-    }
-}
-<?php
-
-namespace App\Entity;
-
-use Doctrine\ORM\Mapping as ORM;
-
-/**
- * @ORM\Entity(repositoryClass="App\Repository\StudentAnswerRepository")
- */
-class StudentAnswer
-{
-    /**
-     * @ORM\Id()
-     * @ORM\GeneratedValue()
-     * @ORM\Column(type="integer")
-     */
-    private $id;
-
-    /**
-     * @ORM\ManyToOne(targetEntity="App\Entity\QuestionInExam", inversedBy="studentAnswers")
-     * @ORM\JoinColumn(nullable=false)
-     */
-    private $questionInExam;
-
     /**
      * @ORM\OneToOne(targetEntity="App\Entity\Assessment", inversedBy="studentAnswer", cascade={"persist", "remove"})
      */
