f5724fce6e2a214fe984781d33eb455cae5a4f0d
siennad <siennad97@gmail.com>
2018-09-30 23:15:08 +0300

    added crud for questions

--- symfonyApp/src/Controller/IndexController.php
+++ symfonyApp/src/Controller/IndexController.php
@@ -15,4 +15,23 @@ class IndexController extends AbstractController
             'controller_name' => 'IndexController',
         ]);
     }
+
+    public function login()
+    {
+        return $this->render('index/index.html.twig', [
+            'controller_name' => 'IndexController',
+            'user_login' => true,
+            'user_role' => 'teacher',
+        ])  ;
+    }
+
+    public function logout()
+    {
+        // if the user haven't logged in: return to login page
+        // if user has logged in, return to homepage (the dashboard)
+        return $this->render('index/index.html.twig', [
+            'controller_name' => 'IndexController',
+        ]);
+    }
+
 }
