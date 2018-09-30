e2ec9b5d35ab62514139651e24af1f00a4a722ae
longnguyendr <long.nguyendr@gmail.com>
2018-10-01 01:07:04 +0300

    error message with login

--- symfonyApp/config/bundles.php
+++ symfonyApp/config/bundles.php
@@ -11,4 +11,5 @@ return [
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
+    Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
 ];
--- symfonyApp/src/DataFixtures/Userfixtures.php
+++ symfonyApp/src/DataFixtures/Userfixtures.php
@@ -7,8 +7,7 @@ use Doctrine\Common\Persistence\ObjectManager;
 
 class Userfixtures extends Fixture
 {
-    public function load(ObjectManager $manager)
-    {
+    public function load(ObjectManager $manager) {
         // $product = new Product();
         // $manager->persist($product);
         
