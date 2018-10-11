32c4f75b0fa20d00a47d826720b42a12d1d9998b
siennad <siennad97@gmail.com>
2018-10-11 10:17:27 +0300

    delete some stuffs

--- symfonyApp/src/Controller/ExamController.php
+++ symfonyApp/src/Controller/ExamController.php
@@ -9,22 +9,14 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+
 /**
  * @Route("/exam")
  */
 class ExamController extends AbstractController
 {
-    public function Dashboard()
-    {
-        $user = $this->getUser();
-        return $this->render('exam/index.html.twig', [
-            'controller_name' => 'ExamController',
-            'user' => $user->getName()
-        ]);
-    }
-
     /**
-     * @Route("/view-all", name="exam_index", methods="GET")
+     * @Route("/", name="exam_index", methods="GET")
      */
     public function index(ExamRepository $examRepository): Response
     {
@@ -32,9 +24,9 @@ class ExamController extends AbstractController
     }
 
     /**
-     * @Route("/create-exam", name="create_new_exam", methods="GET|POST")
+     * @Route("/new", name="exam_new", methods="GET|POST")
      */
-    public function newExam(Request $request): Response
+    public function new(Request $request): Response
     {
         $exam = new Exam();
         $form = $this->createForm(ExamType::class, $exam);
@@ -54,4 +46,45 @@ class ExamController extends AbstractController
         ]);
     }
 
+    /**
+     * @Route("/{id}", name="exam_show", methods="GET")
+     */
+    public function show(Exam $exam): Response
+    {
+        return $this->render('exam/show.html.twig', ['exam' => $exam]);
+    }
+
+    /**
+     * @Route("/{id}/edit", name="exam_edit", methods="GET|POST")
+     */
+    public function edit(Request $request, Exam $exam): Response
+    {
+        $form = $this->createForm(ExamType::class, $exam);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('exam_edit', ['id' => $exam->getId()]);
+        }
+
+        return $this->render('exam/edit.html.twig', [
+            'exam' => $exam,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="exam_delete", methods="DELETE")
+     */
+    public function delete(Request $request, Exam $exam): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$exam->getId(), $request->request->get('_token'))) {
+            $em = $this->getDoctrine()->getManager();
+            $em->remove($exam);
+            $em->flush();
+        }
+
+        return $this->redirectToRoute('exam_index');
+    }
 }
