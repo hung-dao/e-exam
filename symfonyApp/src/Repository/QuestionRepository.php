42f0e0a505bcd5b012f622c41c7e5356d382874c
long.nguyendr <long.nguyendr@gmail.com>
2018-10-14 00:33:43 +0300

    complete add question by category

--- symfonyApp/src/Repository/QuestionRepository.php
+++ symfonyApp/src/Repository/QuestionRepository.php
@@ -57,13 +57,18 @@ class QuestionRepository extends ServiceEntityRepository
         ;
     }
     */
-    public function findQuestionsByCategory($value)
+    /**
+     * @param $value
+     * @param $number
+     * @return mixed
+     */
+    public function findQuestionsByCategory($value, $number)
     {
         return $this->createQueryBuilder('query')
             ->andWhere('query.category = :cat')
             ->setParameter('cat', $value)
             ->orderBy('query.id', 'ASC')
-            ->setMaxResults(10)
+            ->setMaxResults($number)
             ->getQuery()
             ->getResult();
     }
