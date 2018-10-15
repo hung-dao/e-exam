dccd3b8bd70690bf0565dd2da43c2199e0bb6c3e
long.nguyendr <long.nguyendr@gmail.com>
2018-10-16 02:58:48 +0300

    Update fixtures

--- symfonyApp/src/DataFixtures/Userfixtures.php
+++ symfonyApp/src/DataFixtures/Userfixtures.php
@@ -23,12 +23,30 @@ class Userfixtures extends Fixture
         //teacher 2
         $dummyUser = new User();
         $dummyUser->setName("Laura Laitinen");
-
         $dummyUser->setUsername('teacher2');
         $dummyUser->setPassword(password_hash('test_password', PASSWORD_BCRYPT));
         $dummyUser->setRole(1); //1 = teacher;
         $manager->persist($dummyUser);
         $manager->flush();
+
+        //teacher 3
+        $dummyUser = new User();
+        $dummyUser->setName("Laura Teacher3");
+        $dummyUser->setUsername('teacher3');
+        $dummyUser->setPassword(password_hash('test_password', PASSWORD_BCRYPT));
+        $dummyUser->setRole(1); //1 = teacher;
+        $manager->persist($dummyUser);
+        $manager->flush();
+
+        //teacher 4
+        $dummyUser = new User();
+        $dummyUser->setName("Laura Kanna");
+        $dummyUser->setUsername('teacher4');
+        $dummyUser->setPassword(password_hash('test_password', PASSWORD_BCRYPT));
+        $dummyUser->setRole(1); //1 = teacher;
+        $manager->persist($dummyUser);
+        $manager->flush();
+
         //students
         $dummyUser = new User();
         $dummyUser->setName("Ammie Karkkinen");
@@ -51,6 +69,15 @@ class Userfixtures extends Fixture
         $dummyUser = new User();
         $dummyUser->setName("Emmi Karkkinen");
         $dummyUser->setUsername('student3');
+        $dummyUser->setPassword(password_hash('test_password', PASSWORD_BCRYPT));
+        $dummyUser->setRole(0); //0 = student;
+        $manager->persist($dummyUser);
+        $manager->flush();
+
+        //student 3
+        $dummyUser = new User();
+        $dummyUser->setName("Emmi Kottkantie");
+        $dummyUser->setUsername('student4');
         $dummyUser->setPassword(password_hash('test_password', PASSWORD_BCRYPT));
         $dummyUser->setRole(0); //0 = student;
         $manager->persist($dummyUser);
